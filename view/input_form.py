a45ea17e44d4ab287ea30e08b5ef8f1327a9f674
Nabil Putra <nabilputra099123@gmail.com>
2024-12-10 10:18:07 +0700

    Menambahkan File

--- view/input_form.py
+++
@@ -1,7 +0,0 @@
-class InputForm:
-    def input_data(self):
-        print("Masukkan Data Mahasiswa:")
-        id = input("NIM: ")
-        nama = input("Nama: ")
-        jurusan = input("Jurusan: ")
-        return id, nama, jurusan
